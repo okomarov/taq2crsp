aa12a26e594bbbe01418dfaddd6eca18f84e8a73
oleg komarov <o.komarov11@imperial.ac.uk>
2014-04-11 15:56:30 +0100

    Finalized changes to CUSIP8 matching (previous matching buggy)
    Signed-off-by: oleg komarov <o.komarov11@imperial.ac.uk>

--- exportMasterTAQ.m
+++ exportMasterTAQ.m
@@ -1,8 +1,8 @@
 %% TAQmaster for MySQL
-tic
+d = '.\data';
+%% Load .csv
 % List all TAQ .csv master files
-d         = '.\data\raw';
-list      = unzip(fullfile(d,'TAQmast.csv.zip'),d);
+list      = unzip(fullfile(d,'raw','TAQmast.csv.zip'),d);
 list      = sort(list);
 % Due to a change in FDATE as per TAQ readme.txt which screwed the field,exclude 2000/02 and 2000/03 master files
 idx       = cellfun('isempty', regexp(list,'(mst200002|mst200003)','once'));
@@ -22,9 +22,10 @@ for ii = 1:nfiles
     % Eventually rename DATEF to FDATE (in some files it changes)
     TAQmaster{ii}.Properties.VarNames = regexprep(TAQmaster{ii}.Properties.VarNames,'(?i)datef','FDATE');
 end
-
+delete(list{:})
+%% Load .tab 
 % Add .tab master files from dvd
-list2     = unzip(fullfile(d,'TAQmast.tab.zip'),d);
+list2     = unzip(fullfile(d,'raw','TAQmast.tab.zip'),d);
 list2     = sort(list2);
 fmt       = '%10c %30c %12c %c%c%c %*2c %c%c%c%c%c %c %4c %*10c %*4c %c %c %8c %*[^\n]';
 names     = {'SYMBOL','NAME','CUSIP','ETN','ETA','ETB','ETP','ETX','ETT','ETO','ETW','ITS','ICODE','DENOM','TYPE','FDATE'};
@@ -47,22 +48,34 @@ for ii = 1:nfiles2
     TAQmaster{nfiles+ii} = cell2dataset(g(tmp),'VarNames',names);
 end
 toc
+delete(list2{:})
+%% Post-processing
+% NOTE: I decided to avoid additional post-processing on the symbols here,
+% and tackle the multiple SYMBOLs per CUSIP at 'same' FDATE later, after
+% the match with PERMNO has been established. 
+% To understand why this comment is relevant we need to answer to the question
+% "How does the multiple-SYMBOL-same-FDATE problem affect us?"
+% If I query by permno, I will most likely get two or more overlapping
+% price series matched back to TAQ. Also, I cannot edit SYMBOL here to get
+% rid of the secondary series because I want to retain a clean mapping. 
+% Thus, after the mapping has been carried out, I will tag with a unique ID
+% that will into consideration PERMNO, SYMBOL and FDATE.
+% It's actually more complicated than it might appear, due to the numerous
+% suffixes that SYMBOLS have, see the TAQ monthly guide suffix appendices.
 
 % Concatenate everything - 20 sec
 TAQmaster = cat(1,TAQmaster{:});
 
-% Problematic 8-CUSIP
-% prob = TAQmaster(~cellfun('isempty',regexp(TAQmaster.CUSIP,'^0{8}','match','once')),:);
-% [un,~,subs] = unique(prob(:,{'SYMBOL','NAME','CUSIP'}));
-% un.FDATE = accumarray(subs, prob.FDATE,[],@min);
-
 % Extract CUSIP info. CUSIP lengths can be 12 (full), 0 (absent) and 9 (missing NSCC issue digits)
 CUSIP = char(TAQmaster.CUSIP);
+
 % Extract 8-CUSIP
 TAQmaster.CUSIP8 = cellstr(CUSIP(:,1:8));
+
 % Extract the 9th digit/character, i.e. 3rd of the TAQ issue
 tmp = textscan([CUSIP(:,9),repmat(',',size(CUSIP,1),1)]','%f','Delimiter',',');
 TAQmaster.CUSIP9 = tmp{1};
+
 % Extract the last 3 digits, i.e. the NSCC issue:
 % NYSE 000, NYSE at issue 100
 % MKT  001, MKT  at issue 101
@@ -70,39 +83,39 @@ TAQmaster.CUSIP9 = tmp{1};
 tmp = textscan([CUSIP(:,10:12),repmat(',',size(CUSIP,1),1)]','%f','Delimiter',',');
 TAQmaster.NSCC = tmp{1};
 
+% All zero 8-CUSIPs
+idx = strcmp(TAQmaster.CUSIP8,'00000000');
+% prob        = TAQmaster(idx,:);
+% [un,~,subs] = unique(prob(:,{'SYMBOL','NAME','CUSIP8'}));
+% un.FDATE    = accumarray(subs, prob.FDATE,[],@min);
 % Make '00000000' 8-cusips empty
-TAQmaster.CUSIP8(strcmp(TAQmaster.CUSIP8,'00000000')) = {''};
-
-%% Continue form here
+TAQmaster.CUSIP8(idx) = {''};
+%% TAQsymbols
 
 % Export symbols only, necessary for cleanest match with CRSP cusip 
-% --------------
-TAQsymbols = unique(TAQmaster(:,{'CUSIP','SYMBOL','NAME','FDATE'}));
+TAQsymbols = unique(TAQmaster(:,{'CUSIP8','SYMBOL','NAME','FDATE'}));
 
 % Collapse dates for symbols, NOTE the sorting. This is to capure forth/back in the symbol:
 % 00088E10	IATV	ACTV INC	19930104
 % 00088E10	ACTV	ACTV INC	19950503
 % 00088E10	IATV	ACTV INC	19980630
-
-TAQsymbols = sortrows(TAQsymbols,{'CUSIP','FDATE','SYMBOL'});
-idx        =  [true; ~(strcmpi(TAQsymbols.CUSIP(2:end), TAQsymbols.CUSIP(1:end-1)) &...
+TAQsymbols = sortrows(TAQsymbols,{'CUSIP8','FDATE','SYMBOL'});
+idx        =  [true; ~(strcmpi(TAQsymbols.CUSIP8(2:end), TAQsymbols.CUSIP8(1:end-1)) &...
                        strcmpi(TAQsymbols.SYMBOL(2:end), TAQsymbols.SYMBOL(1:end-1)))];
 TAQsymbols = TAQsymbols(idx,:);
 
 % Replace missing with \N, for MySQL's LOAD INFILE
 missing = ismissing(TAQsymbols);
 TAQsymbols.NAME(missing(:,3)) = {'\N'};
-TAQsymbols.CUSIP(missing(:,1)) = {'\N'};
+TAQsymbols.CUSIP8(missing(:,1)) = {'\N'};
+
 % Export
-export(TAQsymbols,'file',fullfile(d,'TAQsymbols.csv'),'Delim','\t') % Remember to change manually to UTF8 encoding
-% ----------------
+export(TAQsymbols,'file',fullfile(d,'TAQsymbols.tab'),'Delim','\t') % Remember to change manually to UTF8 encoding
+%% TAQcodetype
+TAQcodetype = unique(TAQmaster(:,{'CUSIP8','SYMBOL','FDATE','ICODE','TYPE'}));
 
-% Type and Icode
-% ----------------
-TAQcodetype = unique(TAQmaster(:,{'CUSIP','SYMBOL','FDATE','ICODE','TYPE'}));
-
-TAQcodetype = sortrows(TAQcodetype,{'CUSIP','FDATE','SYMBOL'});
-idx         =  [true; ~(strcmpi(TAQcodetype.CUSIP (2:end), TAQcodetype.CUSIP (1:end-1)) &...
+TAQcodetype = sortrows(TAQcodetype,{'CUSIP8','FDATE','SYMBOL'});
+idx         =  [true; ~(strcmpi(TAQcodetype.CUSIP8 (2:end), TAQcodetype.CUSIP8 (1:end-1)) &...
                         strcmpi(TAQcodetype.SYMBOL(2:end), TAQcodetype.SYMBOL(1:end-1)) &...
                         strcmpi(TAQcodetype.ICODE (2:end), TAQcodetype.ICODE (1:end-1)) &...
                                 TAQcodetype.TYPE  (2:end)==TAQcodetype.TYPE  (1:end-1) )];
@@ -110,15 +123,12 @@ TAQcodetype = TAQcodetype(idx,:);
 
 % Replace missing with \N, for MySQL's LOAD INFILE
 missing = ismissing(TAQcodetype);
-TAQcodetype.CUSIP(missing(:,1)) = {'\N'};
+TAQcodetype.CUSIP8(missing(:,1)) = {'\N'};
 TAQcodetype.ICODE(missing(:,4)) = {'\N'};
-export(TAQcodetype,'file',fullfile(d,'TAQcodetype.csv'),'Delim','\t') % Remember to change manually to UTF8 encoding
-% ----------------
-
-delete(list{:})
-delete(list2{:})
+export(TAQcodetype,'file',fullfile(d,'TAQcodetype.tab'),'Delim','\t') % Remember to change manually to UTF8 encoding
 toc
-%% Import tclink
+%% WRDStclink
+% Import .csv
 addpath .\utils\
 d          = '.\data';
 list       = unzip(fullfile(d,'raw','WRDStclink.csv.zip'),d);
@@ -133,11 +143,14 @@ WRDStclink = dataset({WRDStclink{1}, varnames{1}},...
                      {WRDStclink{4}, varnames{4}},...
                      {WRDStclink{5}, varnames{5}});
 WRDStclink = replacedata(WRDStclink, @(x) serial2yyyymmdd(datenum(x,'yyyy-mm-dd')),'DATE');
+
 % Consolidate dates
 WRDStclink = sortrows(WRDStclink,{'PERMNO','CUSIP','DATE','SYMBOL'});
 idx        =  [true; ~(        WRDStclink.PERMNO(2:end) == WRDStclink.PERMNO(1:end-1) &...
                        strcmpi(WRDStclink.CUSIP (2:end), WRDStclink.CUSIP(1:end-1)) &...
                        strcmpi(WRDStclink.SYMBOL(2:end), WRDStclink.SYMBOL(1:end-1)))];
 WRDStclink = WRDStclink(idx,:);
+
+% Export
 export(WRDStclink,'file',fullfile(d,'WRDStclink.tab'),'Delim','\t') % Remember to change manually to UTF8 encoding
 delete(list{:})
