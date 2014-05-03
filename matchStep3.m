4e741112ba1b6f1a04d96ada57249eabf7d71370
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-05-03 23:20:59 +0100

    Only symbol match (no name)
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- matchStep3.m
+++ matchStep3.m
@@ -12,6 +12,7 @@ dburl  = sprintf('jdbc:mysql://localhost:3306/%s', dbname);
 pass   = input('Password: ','s');
 conn   = database(dbname, user, pass, driver, dburl);
 clear pass
+clc
 if ~isconnection(conn), error('Not connected.'), end
 
 % Retrieve final
@@ -29,38 +30,48 @@ crsp = replacedata(crsp, @upper, {'comnam','ticker'});
 close(curs),clear curs, close(conn), clear conn
 save debugstate
 %% Ticker and name match
+N = size(final,1);
+final.score = NaN(N,1);
+
+
 startdt = fix(crsp.namedt/100);
 enddt   = fix(crsp.nameenddt/100);
-ticklen = cellfun('size',crsp.ticker,2);
-
+tic
 for ii = 1:size(final,1)
+    % From TAQ
     symbol = final(ii,:).symbol{1};
-    datef  = final(ii,:).datef;
+    n      = numel(symbol);
+    if n > 5, symbol = symbol(1:5); n = 5; end
     
-    % Has symbol
-    if ~isempty(symbol)
-        % Restrict datef to be in monthly [namedt, nameenddt]
-        date = fix(datef/100);
-        idx  = date >= startdt & date <= enddt;
-        % Create char comparison matrix
-        [unticker,pos] = unique(crsp.ticker(idx));
-        unticker = char(unticker);
-        
-        % Check letter by letter
-        n        = numel(symbol);
-        nchars   = sum(bsxfun(@eq, unticker(:,1:n), symbol),2);
-        maxchars = max(nchars);
-        
-        if maxchars == n
-            disp(1)
-        else
-            
+    datef  = final(ii,:).datef;
+    name   = final(ii,:).name{1};
+    
+    
+    % Restrict datef to be in monthly [namedt, nameenddt]
+    date  = fix(datef/100);
+    idate = date >= startdt & date <= enddt;
+    % Create char comparison matrix
+    tmp     = crsp(idate,{'ticker','comnam','permno'});
+    ticklen = cellfun('size',tmp.ticker,2);
+    cticker = char(tmp.ticker);
+    
+    % Check letter by letter
+    nchars   = sum(bsxfun(@eq, cticker(:,1:n), symbol),2);
+    maxchars = max(nchars);
+    
+    imatch = nchars == maxchars & ticklen == maxchars;
+    if ~isempty(name)
+        comnames = char(tmp.comnam(imatch));
+        % Add selection by name
+    else
+        permno = unique(tmp.permno(imatch));
+        if numel(permno) > 1
+            fprintf('Multiple permnos no name on %d\n',ii)
+        elseif ~isempty(permno)
+            final(ii,:).score = 4;
+            final(ii,:).permno = permno;
         end
-        
-    elseif ~isempty(record.name)
-
     end
 
-    
-    crsp.comnam(1e4)
 end
+toc
